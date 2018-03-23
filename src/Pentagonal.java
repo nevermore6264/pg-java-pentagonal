0940be38c176e19b0df37987538e01165931d38b
nevermore6264 <37317921+nevermore6264@users.noreply.github.com>
2018-03-23 14:07:27 +0700

    [Bài tập] Hiển thị số pentagonal

---
+++ src/Pentagonal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Pentagonal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println("Pentagonal: " + getPentagonalNumber(number));
+    }
+
+    public static int getPentagonalNumber(int n) {
+        return n * (3 * n - 1) / 2;
+    }
+}
